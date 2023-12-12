d86d3012cc42cb24c7428904f1497c48284f4ede
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2023-12-12 20:47:23 +0530

    added gallery section

--- Assetes/js/script.js
+++ Assetes/js/script.js
@@ -8,6 +8,28 @@ function menutoggle() {
         menulist.style.maxHeight = '0px';
     }
 }
+/*--Gallery---*/
+$('.owl-carousel1').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    navText: [" <i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 4
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
 /*--rooms---*/
 $('.owl-carouse').owlCarousel({
     loop: true,
