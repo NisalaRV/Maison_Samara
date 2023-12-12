f583709bc5a3e7aa25628c4e3f7a24d6e545114c
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2023-12-12 15:47:04 +0530

    added Accommodation section

--- Assetes/js/script.js
+++ Assetes/js/script.js
@@ -8,6 +8,26 @@ function menutoggle() {
         menulist.style.maxHeight = '0px';
     }
 }
+/*--rooms---*/
+$('.owl-carouse').owlCarousel({
+    loop: true,
+    margin: 40,
+    nav: true,
+    dots: false,
+    navText: [" <i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2,
+            margin: 10,
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
 
 $('.owl-carousel').owlCarousel({
     loop: true,
@@ -27,3 +47,9 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+
+
+
